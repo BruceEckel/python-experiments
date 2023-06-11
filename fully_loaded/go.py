86e619a43f9ec67aa3f491bc8c02e8e3cd84d922
Bruce Eckel <BruceTEckel@gmail.com>
2023-06-10 19:04:52 -0600

    Update go.py

--- fully_loaded/go.py
+++ fully_loaded/go.py
@@ -1,26 +1,25 @@
 from concurrent.futures import ProcessPoolExecutor
 import math
 import time
-
-MULTIPLIER = 1  # Increase for longer computations
+import os
 
 
 def computation(n):  # CPU-intensive
+    multiplier = 1  # Increase for longer computations
     result = 0
-    for i in range(10**7 * MULTIPLIER):
+    for i in range(10**7 * multiplier):
         result += math.sqrt(i**3 + i**2 + i * n)
     return result
 
 
 if __name__ == "__main__":
     start = time.monotonic()
-    print(f"{MULTIPLIER = }")
+    logical_processors = os.cpu_count()
+    print(f"{logical_processors = }")
+    tasks = (logical_processors - 0) * 1  # Try different numbers of tasks
+    print(f"{tasks = }")
 
     with ProcessPoolExecutor() as executor:
-        logical_processors = executor._max_workers
-        print(f"{logical_processors = }")
-        tasks = (logical_processors - 0) * 1  # Try different numbers of tasks
-        print(f"{tasks = }")
         results = executor.map(computation, range(tasks))
 
     print(list(results))
