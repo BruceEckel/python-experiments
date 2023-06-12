d4c0b356951df429e6746327941f296f1715772c
Bruce Eckel <BruceTEckel@gmail.com>
2023-06-12 13:34:15 -0600

    Removed tokio and num_cpus dependencies

--- fully_loaded/rust/src/main.rs
+++ fully_loaded/rust/src/main.rs
@@ -1,4 +1,5 @@
-use tokio::sync::mpsc;
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread;
 use std::time::Instant;
 
 fn cpu_intensive(n: u128, multiplier: u8) -> f64 {
@@ -9,28 +10,37 @@ fn cpu_intensive(n: u128, multiplier: u8) -> f64 {
     result
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let multiplier: u8 = 1; // Increase for longer computations
-    let logical_processors = num_cpus::get();
+    let logical_processors = thread::available_parallelism().unwrap().get();
     dbg!(logical_processors);
     let tasks = (logical_processors - 0) * 1; // Try different numbers
     dbg!(tasks);
     let timer = Instant::now();
 
-    let (tx, mut rx) = mpsc::channel(tasks);
+    let (tx, rx) = mpsc::channel();
+
+    let rx = Arc::new(Mutex::new(rx));
+    let mut handles = Vec::new();
 
     for i in 0..tasks {
         let tx = tx.clone();
-        tokio::spawn(async move {
+
+        let handle = thread::spawn(move || {
             let result = cpu_intensive(i as u128, multiplier);
-            tx.send(result).await.unwrap();
+            tx.send(result).unwrap();
         });
+
+        handles.push(handle);
     }
 
     drop(tx); // Drop the transmitter to close the channel
 
-    while let Some(result) = rx.recv().await {
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    while let Ok(result) = rx.lock().unwrap().recv() {
         println!("{:?}", result);
     }
 
