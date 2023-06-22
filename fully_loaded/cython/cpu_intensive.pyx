00450bdd2ff679ec212004aa5a67917c273d16d1
Bruce Eckel <BruceTEckel@gmail.com>
2023-06-22 09:54:30 -0600

    Update cpu_intensive.pyx

--- fully_loaded/cython/cpu_intensive.pyx
+++ fully_loaded/cython/cpu_intensive.pyx
@@ -9,3 +9,17 @@ def cpu_intensive(n: cython.ulong, multiplier: cython.ulong) -> cython.double:
         result += math.sqrt(i**3 + i**2 + i * n)
 
     return result
+
+# The above compiles and runs but produces incorrect results, and is about 10x slower than the Rust extension.
+# I also tried the following, which compiles but produces "math domain" errors at runtime:
+
+# import math
+#
+# cpdef double cpu_intensive(long long n, long long multiplier):
+#    cdef double result = 0.0
+#    cdef long long i
+#
+#    for i in range(10_000_000 * multiplier):
+#        result += math.sqrt(i**3 + i**2 + i * n)
+#
+#    return result
