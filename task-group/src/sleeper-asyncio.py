adb6bf1457fc3f03703ba6733b686cbcb90b17dd
Bruce Eckel <mindviewinc@gmail.com>
2023-08-02 17:14:40 -0600

    refined

--- task-group/src/sleeper-asyncio.py
+++ task-group/src/sleeper-asyncio.py
@@ -2,25 +2,32 @@
 import asyncio
 
 
-async def sleeper(id: str, secs: int):
-    print(f"{id} sleeping {secs}s")
+def show_tasks(msg: str):
+    for task in asyncio.all_tasks():
+        running = task.get_coro().cr_running
+        print(
+            f"{task.get_name()}[{running}]",
+            end=" ",
+        )
+    print(f"\n{msg}\n" + "-" * 25)
+
+
+async def nap(id: str, secs: int):
+    show_tasks(f"{id} napping {secs}s")
     await asyncio.sleep(secs)
-    print(f"{id} waking {secs}s")
+    show_tasks(f"{id} woken after {secs}s")
 
 
 async def main():
+    for task in asyncio.all_tasks():
+        if task.get_coro().__name__ == "main":
+            task.set_name("Main")
     async with asyncio.TaskGroup() as tg:
-        tg.create_task(sleeper("a", 5))
-        tg.create_task(sleeper("b", 3))
-        tg.create_task(sleeper("c", 1))
-        for task in asyncio.all_tasks():
-            coro = task.get_coro()
-            print(
-                f"{task.get_name()}: "
-                + f"{coro.__name__}, "
-                + f"{coro.cr_running}"
-            )
-    print("Tasks complete")
+        tg.create_task(nap("A", 5), name="A")
+        tg.create_task(nap("B", 3), name="B")
+        tg.create_task(nap("C", 1), name="C")
+        show_tasks("tasks created")
+    show_tasks("Tasks complete")
 
 
 asyncio.run(main())
