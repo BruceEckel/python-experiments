495541a18b3d1ae493541ece473c20b22108d908
Bruce Eckel <mindviewinc@gmail.com>
2023-08-09 15:37:37 -0600

    reworking

--- cancellation/src/cancellation.py
+++ cancellation/src/cancellation.py
@@ -1,17 +1,28 @@
 # cancellation.py
 import asyncio
-from show_tasks import show_tasks
 from obj import Obj
 from task import task
 
 
-async def main():
-    def stop(taskref):
-        print(
-            f"!!! cancel {taskref.get_name()}"
-        )
-        taskref.cancel()
+def track(
+    stop_task: asyncio.Task | None = None,
+    msg: str = "",
+) -> None:
+    def display(s: str):
+        print(s, end=": ")
 
+    if msg:
+        display("(" + msg + ")")
+    if stop_task:
+        name = stop_task.get_name()
+        display(f"! {name}.cancel()")
+        stop_task.cancel()
+    for t in asyncio.all_tasks():
+        print(f"{t.get_name()}", end=", ")
+    print()
+
+
+async def main():
     async with asyncio.TaskGroup() as tg:
         tasks = [
             tg.create_task(
@@ -19,14 +30,16 @@ async def main():
             )
             for n, id in enumerate("ABCDEF")
         ]
-        show_tasks("Post-creation")
-        stop(tasks[0])
-        show_tasks("After stop(tasks[0])")
-        await asyncio.sleep(0.5)
-        stop(tasks[1])
-        show_tasks("After stop(tasks[1])")
-        stop(tasks[-2])
-        show_tasks("After stop(tasks[-2])")
+        track(stop_task=tasks[0])
+        await asyncio.sleep(0.1)
+        track(msg="After sleep")
+        track(stop_task=tasks[1])
+        await asyncio.sleep(0.1)
+        track(msg="After sleep")
+        track(stop_task=tasks[-1])
+        await asyncio.sleep(0.1)
+        track(msg="After sleep")
+        track(stop_task=tasks[3])
 
 
 asyncio.run(main())
--- cancellation/src/task.py
+++ cancellation/src/task.py
@@ -4,13 +4,13 @@ from obj import Obj
 
 
 async def task(id: str, delay: int):
-    o = Obj(id)
     print(f"starting task({id}, {delay})")
+    o = Obj(id)
     await asyncio.sleep(delay)
     op = Obj(id + "'")
     await asyncio.sleep(1.0)
     opp = Obj(id + "''")
     print(
-        f"ending task({id}, {delay})"
+        f"~task({id}, {delay})"
         + f" containing {o}, {op} & {opp}"
     )
