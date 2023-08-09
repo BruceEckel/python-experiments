757341bce5414942deafb78d08c522697deb0a9c
Bruce Eckel <mindviewinc@gmail.com>
2023-08-09 15:39:57 -0600

    show_tasks -> track

--- cancellation/src/cancellation.py
+++ cancellation/src/cancellation.py
@@ -1,25 +1,7 @@
 # cancellation.py
 import asyncio
-from obj import Obj
 from task import task
-
-
-def track(
-    stop_task: asyncio.Task | None = None,
-    msg: str = "",
-) -> None:
-    def display(s: str):
-        print(s, end=": ")
-
-    if msg:
-        display("(" + msg + ")")
-    if stop_task:
-        name = stop_task.get_name()
-        display(f"! {name}.cancel()")
-        stop_task.cancel()
-    for t in asyncio.all_tasks():
-        print(f"{t.get_name()}", end=", ")
-    print()
+from track import track
 
 
 async def main():
--- cancellation/src/show_tasks.py
+++
@@ -1,9 +0,0 @@
-# show_tasks.py
-import asyncio
-
-
-def show_tasks(msg: str) -> None:
-    print(f"{msg}:", end=" ")
-    for task in asyncio.all_tasks():
-        print(f"{task.get_name()}", end=", ")
-    print()
