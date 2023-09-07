9797f6e4fc16fd4d7eb08440a2987b74bcc1d439
Bruce Eckel <mindviewinc@gmail.com>
2023-09-07 10:18:17 -0600

    Add black line-length to all projects
    using add-black.py generated by chat 4

--- threading/lots_of_threads.py
+++ threading/lots_of_threads.py
@@ -5,13 +5,20 @@ import time
 
 
 if __name__ == "__main__":
-    threads = [Thread(target=lambda: time.sleep(10)) for _ in range(99)]
+    threads = [
+        Thread(target=lambda: time.sleep(10))
+        for _ in range(99)
+    ]
     for t in threads:
         t.start()
-    print(f"{threading.enumerate() = }")
-    print(f"{threading.main_thread() = }")
-    print(f"{threading.main_thread().native_id = }")
-    print(f"{threading.main_thread().is_alive() = }")
-    print(f"{threading.active_count() = }")
+    print(
+        f"""
+    {threading.enumerate() = }
+    {threading.main_thread() = }
+    {threading.main_thread().native_id = }
+    {threading.main_thread().is_alive() = }
+    {threading.active_count() = }
+    """
+    )
     for t in threads:
         t.join()
