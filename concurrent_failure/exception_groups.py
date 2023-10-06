535fab0c0ae8d8b95b6bdeddb57ffafd1813d996
Bruce Eckel <MindviewInc@gmail.com>
2023-10-06 13:57:11 -0600

    exception groups needs conversion to '*'

--- concurrent_failure/exception_groups.py
+++ concurrent_failure/exception_groups.py
@@ -1,43 +1,46 @@
 import asyncio
 
 
-async def fallible(i):
-    # print(f"fallible({i})")
+async def fallible(i: int, print_lock: asyncio.Lock) -> str:
+    async with print_lock:
+        print(f"fallible({i})")
+
     match i:
         case 5:
-            raise ValueError(f"i: {i}")
+            raise ValueError(f"i = {i}")
         case _ if i % 2 == 0:
-            raise TypeError(f"i: {i}")
+            raise TypeError(f"i = {i}")
         case _ if i % 3 == 0:
-            raise AttributeError(f"i: {i}")
+            raise AttributeError(f"i = {i}")
         case _:
             # Convert number to letter:
             return chr(ord("a") + i)
 
 
-async def main():
+async def main() -> None:
+    print_lock = asyncio.Lock()
+    tasks = []
     try:
         async with asyncio.TaskGroup() as tg:
-            tasks = [tg.create_task(fallible(i)) for i in range(8)]
-    except* ValueError as e:
-        print(f"Value Error: '{e}'")
-    except* TypeError as e:
-        print(f"Type Error: '{e}'")
-    except* AttributeError as e:
-        print(f"Attribute Error: '{e}'")
-
+            tasks = [tg.create_task(fallible(i, print_lock)) for i in range(10)]
+    except ExceptionGroup as e:
+        for exc in e.exceptions:
+            match exc:
+                case ValueError() as ve:
+                    print(f"Value Error: '{ve}'")
+                case TypeError() as te:
+                    print(f"Type Error: '{te}'")
+                case AttributeError() as ae:
+                    print(f"Attribute Error: '{ae}'")
+    async with print_lock:
+        print("---> Tasks created")
     for t in tasks:
+        print(f"{t.get_name()}", end=", ")
         try:
-            print(
-                f"name: {t.get_name()}, result: {t.result()}"
-            )  # Or re-raises exception
+            print(f"result = {t.result()}")
         except Exception as e:
-            print(f"name: {t.get_name()}, Exception {e}")
-        # if t.exception:
-        #     print(f"{str(t.exception())}")
-        #     # print(f"{dir(t)}")
-        # if t.result:
-        #     print(f"{str(t.result())}")
+            print(f"{type(e).__name__}[{e}]")
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
