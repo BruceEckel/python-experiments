97750610b7d2e7213dd8afa92b485ec6c7cf906e
Bruce Eckel <MindviewInc@gmail.com>
2023-10-06 13:07:42 -0600

    Update synchronized_output.py

--- concurrent_failure/synchronized_output.py
+++ concurrent_failure/synchronized_output.py
@@ -1,17 +1,14 @@
 import asyncio
-from typing import List, Any
 
 
 class BaseError(Exception):
-    message: str
-
-    def __init__(self, message: str) -> None:
-        self.message = message
-        super().__init__(message)
-
     # When pattern matching, bind
     # to the `message` attribute:
     __match_args__ = ("message",)
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+        self.message = message
 
 
 class FallibleError(BaseError):
@@ -22,9 +19,7 @@ class PanicError(BaseError):
     pass
 
 
-async def fallible(
-    i: int, print_lock: asyncio.Lock
-) -> str | None:  # It can return str or raise an error
+async def fallible_task(i: int, print_lock: asyncio.Lock) -> str:
     async with print_lock:
         print(f"fallible({i})")
 
@@ -32,33 +27,27 @@ async def fallible(
         raise PanicError(f"i:{i} panicked!")
     elif i % 2 == 0:
         raise FallibleError(f"Failed({i})")
-    else:
-        # Convert number to char:
-        return chr(ord("a") + i)
+    return chr(ord("a") + i)
 
 
 async def main() -> None:
     print_lock = asyncio.Lock()
 
-    tasks: List[asyncio.Task[str | None]] = [
-        asyncio.create_task(fallible(i, print_lock)) for i in range(10)
-    ]
+    tasks = [fallible_task(i, print_lock) for i in range(10)]
 
     async with print_lock:
         print("Tasks created")
 
-    results: List[str | BaseError | Any] = await asyncio.gather(
-        *tasks, return_exceptions=True
-    )  # Can be string, one of the exceptions, or any other unexpected result
+    results = await asyncio.gather(*tasks, return_exceptions=True)
 
     for result in results:
         match result:
             case str(letter):
                 print(f"Letter: {letter}")
-            case FallibleError(error_msg):
-                print(f"Err: {error_msg}")
-            case PanicError(panic_msg):
-                print(f"Panic: {panic_msg}")
+            case FallibleError(message):
+                print(f"Err: {message}")
+            case PanicError(message):
+                print(f"Panic: {message}")
             case _:
                 print(f"Unexpected: {result}")
 
